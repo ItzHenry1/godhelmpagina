d6752665e0eaa9a6e57d2a0f9a331a89f9b9cf97
itzHenry1 <trscrus@gmail.com>
2024-12-30 17:46:15 -0500

    test

--- script.js
+++ script.js
@@ -23,3 +23,26 @@ const button2 = document.querySelector('#botonbedrock')
 button2.addEventListener('click', () => {
     window.location.href = 'minecraft://?addExternalServer=Godhelm|bedrock.godhelmmc.com:19187';
 })
+
+const serverIp = "play.godhelmmc.com";
+const statusElement = document.getElementById("status");
+
+async function fetchServerStatus() {
+  try {
+    const response = await fetch(`https://api.mcstatus.io/v2/status/java/${serverIp}`);
+    const data = await response.json();
+
+    if (data.online) {
+      statusElement.textContent = `${data.players.online}/${data.players.max}`;
+      statusElement.classList.add("online");
+    } else {
+      statusElement.textContent = "El servidor no está en linea..";
+      //statusElement.classList.add("error");
+    }
+  } catch (error) {
+    statusElement.textContent = "0";
+    //statusElement.classList.add("error");
+  }
+}
+
+fetchServerStatus();
